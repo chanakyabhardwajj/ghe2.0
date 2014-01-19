df592a6a7b364e6f0b492965963de1d2ab2c9548
Chanakya Bhardwaj <chanakyabhardwajj@gmail.com>
2014-01-19 18:34:53 +0530

    imagemin added

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,12 +29,23 @@ module.exports = function(grunt) {
             all: {
                 src: ['js/script.js']
             }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'imagesRaw/home/gallery',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/home/gallery/'
+                }]
+            }
         }
     });
 
     //Load NPM tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 
     //Making grunt default to force in order not to break the project.
