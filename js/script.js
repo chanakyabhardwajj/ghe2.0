43c53ce16891ebf1ef20243819e449355b671ee6
Chanakya Bhardwaj <chanakyabhardwajj@gmail.com>
2014-02-20 19:53:39 +0530

    wip

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
 window.onload = function(){
     //Load the templates & fix the nav-active
+    $('[data-toggle=offcanvas]').click(function () {
+        $('.row-offcanvas').toggleClass('active');
+        $('#sidebar').removeClass('hide');
+    });
 
     $('#sidebar').load('partials/sidebar.html');
 
-    $('#header').load('partials/header.html', function(){
-        $('[data-toggle=offcanvas]').click(function () {
-            $('.row-offcanvas').toggleClass('active');
-            $('#sidebar').removeClass('hide');
-        });
 
-        var pageURL = window.location.pathname.substr(1,window.location.pathname.length-6);
-        if(pageURL){
-            if(pageURL==="2013" || pageURL === "2014"){
-                $("#header .expeditions").addClass("active");
-            }
-            else{
-                $("#header ." + pageURL).addClass("active");
-            }
-        }
-        else{
-            $("#header .index").addClass("active");
-        }
-    });
-    $('#footer').load('partials/footer.html');
     $('#myModal').load('partials/joinmodal.html');
     $('#myUndecidedModal').load('partials/undecidedmodal.html');
 
